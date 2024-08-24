bd2886aaae7cea86264118502a74e85d6cd06958
krabinator <92385804+krabinator@users.noreply.github.com>
2024-08-24 02:25:27 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,9 @@
 Citizen.CreateThread(function()
-    for Index, Value in pairs(Config.Commands) do
-        if Value.Aliases then
-            for _, Alias in pairs(Value.Aliases) do
+    for Index, Array in pairs(Config.Commands) do
+        if Array.Aliases then
+            for _, Alias in pairs(Array.Aliases) do
                 TriggerEvent('chat:addSuggestion', '/' .. Alias, nil, {
-                    {name = Value.Suggestion[1], help = Value.Suggestion[2]}
+                    {name = Array.Suggestion[1], help = Array.Suggestion[2]}
                 })
             end
         end
